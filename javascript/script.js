70c66422ce160fc89d52cda2e57815d23d3b2b0e
KaiqueFj <kaiquelferraz@hotmail.com>
2022-05-24 06:43:15 -0300

    added functions and conditions

--- javascript/script.js
+++ javascript/script.js
@@ -1,60 +1,85 @@
-var canvas = document.getElementById('space-game');
-var ctx = canvas.getContext('2d');
-//definir o inicio do quadrado
-var x = 55;
-var x_largura = 40;
-var y = 280;
-var y_largura = 40;
-var dx = 5;
-var dy = 5;
+var canvas = document.getElementById("space-game");
+var ctx = canvas.getContext("2d");
+
+//dados do quadrado vermelho
+var x_vermelho = 0;
+var y_vermelho = 50;
+var largura_vermelho = 20;
+var altura_vermelho = 20;
+
+//dados do quadrado azul
+var ship_x = 260;
+var ship_y = 50;
+var largura_azul = 15;
+var altura_azul = 15;
 
 requestAnimationFrame(gameloop);
 
-function detectarColisao() {
-    if ((x + x_largura) > (950, 0)) {
-        //interrompe o game loop parando a movimentação dos quadrados
+
+
+function detectarColisao() 
+{
+    if ( ( (x_vermelho + largura_vermelho) > ship_x && x_vermelho < (ship_x + largura_azul) ) &&
+         ( (y_vermelho + altura_vermelho)  > ship_y && y_vermelho < (ship_y + altura_azul)  ) ) 
+         
+        {
+        //interrompe o game loop parando a movimentação
         clearTimeout();
-    }
+        }
     else {
-        //chama novamente o ciclo da animação
+        //chama novamente a animação
         requestAnimationFrame(gameloop);
     }
 }
 
-function gameloop() {
-    desenharQuadrado(x, y);
+function gameloop() 
+{
+    desenharQuadrado(x_vermelho, y_vermelho);
     detectarColisao();
+   
 }
 
 
-function desenharQuadrado(x, y) {
-    ctx.clearRect(0, 0, 0, 0); //antes de fazer o desenho é preciso limpar o canvas
-    ctx.fillStyle = '#00F';
-    ctx.fillRect(x, y, x_largura, y_largura);
-}
 
-function gameloop() {
-    desenharQuadrado(x, y);
-    //chama novamente o ciclo da animação
-    requestAnimationFrame(gameloop);
+
+function desenharQuadrado() 
+{
+    //limpa todo o canvas
+    ctx.clearRect(0, 0, 800, 400);
+
+    //definindo cor vermelha
+    ctx.fillStyle = "rgb(255,0,0)";
+    y_vermelho = Math.floor(Math.random() *400)+1;
+    x_vermelho = Math.floor(Math.random() *300)+1;
+
+    
+    //desenha o quadrado vermelho
+    ctx.fillRect(x_vermelho, y_vermelho, largura_vermelho, altura_vermelho);
+
+    //definindo cor ship_y
+    ctx.fillStyle = "rgb(0,0,255)";
+
+    //desenha o quadrado azul
+    ctx.fillRect(ship_x, ship_y, largura_azul, altura_azul);
+
+    //movimenta o quadrado vermelho
+    x_vermelho = x_vermelho + 0.5;
 }
 
 window.onkeydown = pressionaTecla;
 
-var velocidade = 2;
 
 function pressionaTecla(tecla) {
     if (tecla.keyCode == 39) {
-        x = x + 5; //aumentar o x tem o efeito de ir para a direita     
+        ship_x = ship_x + 5; //aumentar o x tem o efeito de ir para a direita     
     }
     if (tecla.keyCode == 37) {
-        x = x - 5; //diminuir o x tem o efeito de ir para a esquerda
+        ship_x = ship_x - 5; //diminuir o x tem o efeito de ir para a esquerda
     }
     if (tecla.keyCode == 38) {
-        y = y - 5;
+        ship_y = ship_y - 5;
     }
     if (tecla.keyCode == 40) {
-        y = y + 5;
+        ship_y = ship_y + 5;
     }
 }
-
